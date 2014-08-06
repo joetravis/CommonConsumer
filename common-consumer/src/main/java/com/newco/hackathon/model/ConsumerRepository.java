1e1d6f4997f45dafbb1978747f879f81023860d5
travisj <joe.travis@adp.com>
2014-08-06 11:29:50 -0700

    Remove my repository

--- common-consumer/src/main/java/com/newco/hackathon/model/ConsumerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.newco.hackathon.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by travisj on 8/6/14.
- */
-public interface ConsumerRepository extends CrudRepository<Consumer, Long>{
-}
