528faccfaf5c8a7a4d82de5b7d5b398525beffa8
travisj <joe.travis@adp.com>
2014-08-06 19:43:48 -0700

    Adding email match

--- common-consumer/src/main/java/com/newco/hackathon/matching/rule/Email.java
+++ common-consumer/src/main/java/com/newco/hackathon/matching/rule/Email.java
@@ -2,15 +2,37 @@ package com.newco.hackathon.matching.rule;
 
 import com.newco.hackathon.model.Consumer;
 import com.newco.hackathon.model.Match;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
 /**
  * Created by travisj on 8/6/14.
  */
-public class Email implements MatchRule {
+@Service
+public class Email extends AbstractMatchRule {
     @Override
     public List<Match> match(final Consumer consumer) {
-        return null;
+        List<Match> matches = new ArrayList<>();
+        SearchQuery searchQuery;
+        try {
+            searchQuery = new NativeSearchQueryBuilder()
+                    .withQuery(matchQuery("email", consumer.getEmail()))
+                    .withPageable(new PageRequest(0, 10))
+                    .build();
+
+            extractMatchesFromQuery(matches, searchQuery);
+        } catch (Exception e) {
+            return matches;
+        }
+
+        return matches;
     }
 }
