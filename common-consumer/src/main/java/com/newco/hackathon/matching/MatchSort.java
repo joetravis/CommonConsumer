1f57038fc150b3de4259296036d647ac59cf7fe8
John Kelly <wablam@gmail.com>
2014-08-06 18:55:25 -0700

    Reverse match sorting

--- common-consumer/src/main/java/com/newco/hackathon/matching/MatchSort.java
+++ common-consumer/src/main/java/com/newco/hackathon/matching/MatchSort.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class MatchSort implements Comparator<Match> {
     @Override
     public int compare(Match match1, Match match2) {
-        return match1.getSimilarity().compareTo(match2.getSimilarity());
+        return -(match1.getSimilarity().compareTo(match2.getSimilarity()));
     }
 }
